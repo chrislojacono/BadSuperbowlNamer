4f91beefa5bbf305cd7d43780f3a7bfd88d6a53d
Chris LoJacono <clojacono92@gmail.com>
2021-04-05 20:12:01 -0500

    Assert

--- BadSuperBowlNamer.Tests/UnitTest1.cs
+++ BadSuperBowlNamer.Tests/UnitTest1.cs
@@ -7,8 +7,19 @@ namespace BadSuperBowlNamer.Tests
     public class TranslateNumbersToRomanNumerals 
     {
         [Fact]  //this method is a test
-        public void when_the_number_1_is_passed_I_should_be_returned() // must be public and return nothing and take no parameters
+        public void when_the_number_1_is_passed_I_should_be_returned() // must be public and return nothing(void) and take no parameters
         {
+            //Arrange - what are the things I need in order to run these tests -> context creation
+            var numberToConvert = 1;
+            var translator = new RomanNumeralTranslator();
+
+
+            //Act
+            var result = translator.NumberToNumeral(numberToConvert);
+
+            //Assert
+            Assert.Equal("I", result);
+            
 
         }
     }
