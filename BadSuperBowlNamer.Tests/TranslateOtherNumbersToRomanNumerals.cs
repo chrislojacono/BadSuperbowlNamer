c2c9b5876d9d31f12f650857c603e2e6ac5eefae
Chris LoJacono <clojacono92@gmail.com>
2021-04-06 18:34:43 -0500

    theory

---
+++ BadSuperBowlNamer.Tests/TranslateOtherNumbersToRomanNumerals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BadSuperBowlNamer.Tests
+{
+    class TranslateOtherNumbersToRomanNumerals
+    {
+        [Theory]
+        public void the_number_is_translated_correctly_to_roman_numerals()
+        {
+
+        }
+    }
+}
